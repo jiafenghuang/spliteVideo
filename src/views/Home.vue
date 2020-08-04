98492c38ca422d7e8bd944b0c8ca32700a6de7dd
jiafenghuang <39021705+jiafenghuang@users.noreply.github.com>
2020-08-04 23:14:00 +0800

    again

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <video />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import video from "@/components/video.vue";
+export default {
+    name: "Home",
+    components: {
+        video
+    }
+};
+</script>
